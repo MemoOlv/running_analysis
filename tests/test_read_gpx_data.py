74c2c0e120e20886faa8d82aa7f73500b133cc85
MemoOlv <guolg@ier.unam.mx>
2022-08-29 21:23:04 -0500

    🗃️🧪 Update tests

--- tests/test_read_gpx_data.py
+++ tests/test_read_gpx_data.py
@@ -4,5 +4,5 @@ def test_read_gpx_file():
     gpx_file_path = "data/medio_maraton_ensenada.gpx"
     obtained_gpx = read_gpx_file(gpx_file_path)
     obtained_number_of_tracks = obtained_gpx.get_track_points_no()
-    expected_number_of_tracks = 835
+    expected_number_of_tracks = 1730
     assert obtained_number_of_tracks == expected_number_of_tracks

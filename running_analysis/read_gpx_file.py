9f080fdd2255de9e4897c9c89a871ca1256307a0
MemoOlv <guolg@ier.unam.mx>
2022-08-29 21:49:04 -0500

    ✅ Pass test

--- running_analysis/read_gpx_file.py
+++ running_analysis/read_gpx_file.py
@@ -5,3 +5,6 @@ def read_gpx_file(gpx_path):
     with open(gpx_path) as gpx_file:
         gpx = gpxpy.parse(gpx_file)
     return gpx
+
+def get_gpx_segment_points(gpx_object):
+    return gpx_object.tracks[0].segments[0].points

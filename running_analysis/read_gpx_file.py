5fa085a167e491d61b3b4775220687e16da6fe39
MemoOlv <guolg@ier.unam.mx>
2022-08-29 21:26:18 -0500

    ♻️ Make format

--- running_analysis/read_gpx_file.py
+++ running_analysis/read_gpx_file.py
@@ -1,6 +1,7 @@
 import gpxpy
 
-def read_gpx_file(gpx_file_path):
-    with open(gpx_file_path) as gpx_file:
+
+def read_gpx_file(gpx_path):
+    with open(gpx_path) as gpx_file:
         gpx = gpxpy.parse(gpx_file)
     return gpx
--- tests/test_read_gpx_data.py
+++ tests/test_read_gpx_data.py
@@ -1,8 +1,9 @@
 from running_analysis import read_gpx_file
 
+
 def test_read_gpx_file():
-    gpx_file_path = "data/medio_maraton_ensenada.gpx"
-    obtained_gpx = read_gpx_file(gpx_file_path)
+    gpx_path = "data/medio_maraton_ensenada.gpx"
+    obtained_gpx = read_gpx_file(gpx_path)
     obtained_number_of_tracks = obtained_gpx.get_track_points_no()
     expected_number_of_tracks = 1730
     assert obtained_number_of_tracks == expected_number_of_tracks

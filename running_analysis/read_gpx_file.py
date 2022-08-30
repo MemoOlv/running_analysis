5419f1a51e1ce787a057182827bf48cbcbaaae54
MemoOlv <guolg@ier.unam.mx>
2022-08-29 22:44:17 -0500

    ✅ Pass test

--- running_analysis/read_gpx_file.py
+++ running_analysis/read_gpx_file.py
@@ -1,4 +1,5 @@
 import gpxpy
+import pandas as pd
 
 
 def read_gpx_file(gpx_path):
@@ -6,10 +7,24 @@ def read_gpx_file(gpx_path):
         gpx_object = gpxpy.parse(gpx_file)
     return gpx_object
 
+
 def get_gpx_segment_points(gpx_object):
     return gpx_object.tracks[0].segments[0].points
 
+
 def get_route_list_of_dictionaries(gpx_path):
     gpx_object = read_gpx_file(gpx_path)
-    gpx_points = get_gpx_segment_points(gpx_object) 
-    return [{"latitude": point.latitude, "longitude": point.longitude, "elevation": point.elevation, "time": point.time} for point in gpx_points]
+    gpx_points = get_gpx_segment_points(gpx_object)
+    return [
+        {
+            "latitude": point.latitude,
+            "longitude": point.longitude,
+            "elevation": point.elevation,
+            "time": point.time,
+        }
+        for point in gpx_points
+    ]
+
+
+def get_route_dataframe(route_points):
+    return pd.DataFrame(route_points)

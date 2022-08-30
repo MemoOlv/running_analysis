6441abce7d0206ba6255c7cbabb2814ac95b3c7f
MemoOlv <guolg@ier.unam.mx>
2022-08-30 00:55:12 -0500

    🎨 Make format

--- src/half_marathon_ensenada_dataframe.py
+++ src/half_marathon_ensenada_dataframe.py
@@ -6,10 +6,14 @@ app = typer.Typer()
 default_gpx_path = "data/gpx/medio_maraton_ensenada.gpx"
 default_csv_path = "data/processed/half_maraton_ensenada.csv"
 
+
 @app.command()
-def write_csv(gpx_path: str = typer.Argument(default_gpx_path), csv_path: str = typer.Argument(default_csv_path)):
+def write_csv(
+    gpx_path: str = typer.Argument(default_gpx_path),
+    csv_path: str = typer.Argument(default_csv_path),
+):
     write_route_csv(gpx_path, csv_path)
+
 
 if __name__ == "__main__":
     app()
-    

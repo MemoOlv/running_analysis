021952aba54f982ff743338abfcb4db4987fc82a
MemoOlv <guolg@ier.unam.mx>
2022-08-30 00:52:42 -0500

    ✅ Change implementation

--- src/half_marathon_ensenada_dataframe.py
+++ src/half_marathon_ensenada_dataframe.py
@@ -1,4 +1,4 @@
-from running_analysis import get_route_dataframe
+from running_analysis import write_route_csv
 import typer
 
 app = typer.Typer()
@@ -7,9 +7,8 @@ default_gpx_path = "data/gpx/medio_maraton_ensenada.gpx"
 default_csv_path = "data/processed/half_maraton_ensenada.csv"
 
 @app.command()
-def write_route_csv(gpx_path: str = typer.Argument(default_gpx_path), csv_path: str = typer.Argument(default_csv_path)):
-    route_dataframe = get_route_dataframe(gpx_path)
-    route_dataframe.to_csv(csv_path, index=False)
+def write_csv(gpx_path: str = typer.Argument(default_gpx_path), csv_path: str = typer.Argument(default_csv_path)):
+    write_route_csv(gpx_path, csv_path)
 
 if __name__ == "__main__":
     app()

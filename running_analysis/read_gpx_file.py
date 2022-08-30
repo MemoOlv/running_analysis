9466080ed7e3631762e2ae712311b21c86f94a71
MemoOlv <guolg@ier.unam.mx>
2022-08-29 21:51:11 -0500

    🚚 Rename variable

--- running_analysis/read_gpx_file.py
+++ running_analysis/read_gpx_file.py
@@ -3,8 +3,8 @@ import gpxpy
 
 def read_gpx_file(gpx_path):
     with open(gpx_path) as gpx_file:
-        gpx = gpxpy.parse(gpx_file)
-    return gpx
+        gpx_object = gpxpy.parse(gpx_file)
+    return gpx_object
 
 def get_gpx_segment_points(gpx_object):
     return gpx_object.tracks[0].segments[0].points

ced157dbbc2072c5c72321ce080bd7b9d62c121b
MemoOlv <guolg@ier.unam.mx>
2022-09-08 23:09:52 -0500

    🟢 Follow mypy

--- running_analysis/read_gpx_file.py
+++ running_analysis/read_gpx_file.py
@@ -1,7 +1,7 @@
 import gpxpy
 import pandas as pd
 
-from running_analysis.get_speed import add_speed_to_dataframe, get_pace_from_speed
+from .get_speed import add_speed_to_dataframe, get_pace_from_speed
 
 
 def read_gpx_file(gpx_path):

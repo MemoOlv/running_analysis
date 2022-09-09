354970e247296f65775bc264b35678600e74158e
MemoOlv <guolg@ier.unam.mx>
2022-09-08 23:07:09 -0500

    🎨 Make format

--- running_analysis/get_speed.py
+++ running_analysis/get_speed.py
@@ -4,8 +4,9 @@ from geopy import distance
 
 def get_pace_from_speed(route_dataframe):
     route_dataframe_with_speed = add_speed_to_dataframe(route_dataframe)
-    route_dataframe_with_speed["pace"] = (100/6)*(1/route_dataframe_with_speed["speed"]) 
+    route_dataframe_with_speed["pace"] = (100 / 6) * (1 / route_dataframe_with_speed["speed"])
     return route_dataframe_with_speed
+
 
 def add_speed_to_dataframe(route_dataframe):
     list_of_speeds = get_speed_from_dataframe(route_dataframe)
@@ -13,13 +14,18 @@ def add_speed_to_dataframe(route_dataframe):
     route_dataframe["speed"] = list_of_speeds
     return route_dataframe
 
+
 def get_speed_from_dataframe(route_dataframe):
-    return [get_speed_in_meters_per_second(route_dataframe.iloc[i], route_dataframe.iloc[i+1]) for i in range(len(route_dataframe)-1)]
+    return [
+        get_speed_in_meters_per_second(route_dataframe.iloc[i], route_dataframe.iloc[i + 1])
+        for i in range(len(route_dataframe) - 1)
+    ]
+
 
 def get_speed_in_meters_per_second(initial_point, final_point):
     distance = get_distance_in_meters_from_geographic_point(initial_point, final_point)
     time = get_time(initial_point, final_point)
-    return distance/time
+    return distance / time
 
 
 def get_distance_in_meters_from_geographic_point(initial_point, final_point):
@@ -30,6 +36,7 @@ def get_distance_in_meters_from_geographic_point(initial_point, final_point):
     distance_2d = distance.distance(initial_point_lat_lon, final_point_lat_lon).m
     distance_3d = np.sqrt(distance_2d**2 + (final_point_elevation - initia_point_elevation) ** 2)
     return distance_3d
+
 
 def get_time(initial_point, final_point):
     return (final_point["time"] - initial_point["time"]).total_seconds()
--- tests/test_get_speed.py
+++ tests/test_get_speed.py
@@ -15,11 +15,13 @@ route_dataframe = get_route_dataframe(gpx_path).iloc[0:10]
 initial_point = route_dataframe.iloc[3]
 final_point = route_dataframe.iloc[4]
 
+
 def test_add_speed_to_dataframe():
     dataframe_with_speed = add_speed_to_dataframe(route_dataframe)
     expected_first_speed = dataframe_with_speed["speed"].iloc[0]
     obtained_first_speed = 0
     assert obtained_first_speed == expected_first_speed
+
 
 def test_get_pace_from_speed():
     obtained_speed = add_speed_to_dataframe(route_dataframe)
@@ -30,6 +32,7 @@ def test_get_pace_from_speed():
     obtained_first_pace = obtained_pace["pace"].iloc[3]
     expected_first_pace = 0.5621480475337237
     assert obtained_first_pace == expected_first_pace
+
 
 def test_get_speed_from_dataframe():
     obtained_speed = get_speed_from_dataframe(route_dataframe)
@@ -43,15 +46,18 @@ def test_get_speed_from_dataframe():
     expected_first_speed = 14.695700224768062
     assert obtained_first_speed == expected_first_speed
 
+
 def test_get_speed_in_meters_per_second():
     obtained_speed = get_speed_in_meters_per_second(initial_point, final_point)
-    expected_speed = 14.695700224768062/1.0
+    expected_speed = 14.695700224768062 / 1.0
     assert obtained_speed == expected_speed
+
 
 def test_get_distance_in_meters_from_geographic_point():
     obtained_distance = get_distance_in_meters_from_geographic_point(initial_point, final_point)
     expected_distance = 14.695700224768062
     assert obtained_distance == expected_distance
+
 
 def test_get_time():
     obtained_time = get_time(initial_point, final_point)

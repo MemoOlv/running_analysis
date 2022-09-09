78e12c535a149092d11183f51edd4c553d37975d
MemoOlv <guolg@ier.unam.mx>
2022-09-08 20:32:12 -0500

    🧪 Add failing test

--- tests/test_get_speed.py
+++ tests/test_get_speed.py
@@ -15,9 +15,9 @@ final_point = route_dataframe.iloc[4]
 
 def test_get_speed_from_dataframe():
     obtained_speed = get_speed_from_dataframe(route_dataframe)
-    obtained_first_speed = obtained_speed.iloc[0]
-    expected_first_speed = 14.695700224768062
-    assert obtained_first_speed == expected_first_speed
+    obtained_length_speed_dataframe = len(obtained_speed)
+    expected_length_speed_dataframe = 1730
+    assert obtained_length_speed_dataframe == expected_length_speed_dataframe
 
 def test_get_speed():
     obtained_speed = get_speed(initial_point, final_point)

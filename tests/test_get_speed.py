079f661a0d9cd12dabf0caad5ef02b0c6e381acc
MemoOlv <guolg@ier.unam.mx>
2022-09-08 19:46:06 -0500

    🧪 Add failing test

---
+++ tests/test_get_speed.py
@@ -0,0 +1,23 @@
+from running_analysis import (
+    get_route_dataframe,
+    get_speed,
+    get_distance_in_meters_from_geographic_point,
+)
+
+
+gpx_path = "data/gpx/medio_maraton_ensenada.gpx"
+route_dataframe = get_route_dataframe(gpx_path)
+
+
+def test_get_speed():
+    obtained_speed = get_speed(route_dataframe)
+    expected_speed = 4
+    assert obtained_speed == expected_speed
+
+
+def test_get_distance_in_meters_from_geographic_point():
+    initial_point = route_dataframe.iloc[3]
+    final_point = route_dataframe.iloc[4]
+    obtained_distance = get_distance_in_meters_from_geographic_point(initial_point, final_point)
+    expected_distance = 14.695700224768062
+    assert obtained_distance == expected_distance

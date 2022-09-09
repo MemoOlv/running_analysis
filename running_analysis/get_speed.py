33a2c31fc90e4619a6d402ef3da4fa2408411cda
MemoOlv <guolg@ier.unam.mx>
2022-09-08 22:32:10 -0500

    ✅ ♻️ Pass tests and make variables explicit

--- running_analysis/get_speed.py
+++ running_analysis/get_speed.py
@@ -4,7 +4,7 @@ from geopy import distance
 
 
 def get_speed_from_dataframe(route_dataframe):
-    return route_dataframe
+    return [get_speed_in_meters_per_second(route_dataframe.iloc[i], route_dataframe.iloc[i+1]) for i in range(len(route_dataframe)-1)]
 
 def get_speed_in_meters_per_second(initial_point, final_point):
     distance = get_distance_in_meters_from_geographic_point(initial_point, final_point)
@@ -13,13 +13,13 @@ def get_speed_in_meters_per_second(initial_point, final_point):
 
 
 def get_distance_in_meters_from_geographic_point(initial_point, final_point):
-    initial_point_lat_lon = initial_point[:2]
-    initia_point_elevation = initial_point[2]
-    final_point_elevation = final_point[2]
-    final_point_lat_lon = final_point[:2]
+    initial_point_lat_lon = initial_point[["latitude", "longitude"]]
+    final_point_lat_lon = final_point[["latitude", "longitude"]]
+    initia_point_elevation = initial_point["elevation"]
+    final_point_elevation = final_point["elevation"]
     distance_2d = distance.distance(initial_point_lat_lon, final_point_lat_lon).m
     distance_3d = np.sqrt(distance_2d**2 + (final_point_elevation - initia_point_elevation) ** 2)
     return distance_3d
 
 def get_time(initial_point, final_point):
-    return (final_point.time - initial_point.time).total_seconds()
+    return (final_point["time"] - initial_point["time"]).total_seconds()
